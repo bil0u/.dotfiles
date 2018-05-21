a3795a39c7df0450974f8002f2c1b0ac0e0296d5
Ugo Popée <ugo.popee@me.com>
2018-05-21 21:21:28 +0200

    Update aliases.zsh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,22 +1,23 @@
 #!/bin/zsh
 
+$dotdir="~/.dotfiles
+
 # Print all personnal functions informations
 alias dhelp="grep "^function" ~/.dotfiles/functions.sh | sed 's/function/+/g' | sed 's/() #/ - /g'"
+
+# `mv` aliases
+alias mv="mv -i"
 
 # `cd` aliases
 alias dl="cd ~/Downloads"
 alias dt="cd ~/Desktop"
 alias dev="cd ~/Projets"
 
-# `git` aliases
-alias ga="git add"
-alias gcom="git commit -m"
-alias gp="git push"
-
 # `dotfiles` aliases
-alias dotreset="bash -c \"$(curl -fsSL https://raw.github.com/bil0u/.dotfiles/master/install.sh)\""
-alias zshconfig="vim ~/.zshrc"
-alias atdot="atom ~/.dotfiles"
-alias opdot="open ~/.dotfiles"
-
-alias src="source ~/.zshrc"
+alias dotup="bash -c \"$(curl -fsSL https://raw.github.com/bil0u/.dotfiles/master/install.sh)\""
+alias resource="source ~/.zshrc"
+alias cfg_zshrc="vim ~/.zshrc"
+alias cfg_funct="vim $dotdir/zsh/functions.zsh"
+alias cfg_alias="vim $dotdir/zsh/aliases.zsh"
+alias atdot="atom $dotdir"
+alias opdot="open $dotdir"
