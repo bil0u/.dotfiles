a8c347ab70bf4708643ce0b583f6ae7b10b0d43b
Ugo Popée <ugo.popee@me.com>
2018-05-21 08:57:04 +0200

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -58,11 +58,11 @@ function link_dotfiles()
 		if [ -f $homelink ]
 		then
 			mv $homelink $old_dotfiles
-			clr_yellow "[MOVED]  " -n; clr_cyan "$homelink moved to " -n; clr_reset "$old_dotfiles"
+			clr_yellow "[>]      " -n; clr_cyan "$homelink moved to " -n; clr_reset "$old_dotfiles"
 		elif [ -L $homelink ]
 		then
 			rm -f $homelink
-			clr_red "[DELETE] " -n; clr_reset "Symlink" -n; clr_cyan " $homelink " -n; clr_reset "deleted"
+			clr_red "[X]      " -n; clr_reset "Symlink" -n; clr_cyan " $homelink " -n; clr_reset "deleted"
 		fi
 		ln -sf $dotfile $homelink
 		clr_green "[LINKED] " -n; clr_reset "$dotfile to " -n; clr_cyan "$homelink"
