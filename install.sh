09fe196d4960272c9ab8f40bccb99d2808b9ca65
Ugo Popée <ugo.popee@me.com>
2018-05-22 00:01:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -121,9 +121,9 @@ echo $ZSH | grep "oh-my-zsh" > /dev/null 2> /dev/null
 if [ $? -ne 0 ]
 then
 	step "Installing oh-my-zsh"
-	bash $ZSH/tools/install.sh
+	zsh $ZSH/tools/install.sh
 else
 	step "Finished, now start using oh-my-zsh"
-	bash $ZSH/tools/upgrade.sh
-	bash $ZSH/oh-my-zsh.sh
+	zsh $ZSH/tools/upgrade.sh
+	zsh $ZSH/oh-my-zsh.sh
 fi
