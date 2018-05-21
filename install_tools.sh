37290b4032cd9ebdefc69013d9d8690af7085832
Ugo Popée <ugo.popee@me.com>
2018-05-21 22:02:49 +0200

    Update install_tools.sh

--- install_tools.sh
+++ install_tools.sh
@@ -5,7 +5,7 @@
 
 function step()
 {
-	clr_yellow "# " -n; clr_reset "$1"
+	clr_yellow "## " -n; clr_reset "$1"
 }
 
 function install() # Test if arg1 exists with arg2 command, if not if wil install it with arg3..argX commands
@@ -13,7 +13,7 @@ function install() # Test if arg1 exists with arg2 command, if not if wil instal
 	eval $2 > /dev/null 2> /dev/null
 	if [ $? -ne 0 ]
 	then
-		clr_yellow "> " -n; clr_reset "Installing '" -n; clr_cyan "$1" -n; clr_reset "' ..."
+		clr_cyan ">  " -n; clr_reset "Installing '" -n; clr_magenta "$1" -n; clr_reset "' ..."
 		for cmd in "$@"
 		do
 			if [ "$cmd" != "$1" ] && [ "$cmd" != "$2" ]
@@ -21,14 +21,14 @@ function install() # Test if arg1 exists with arg2 command, if not if wil instal
 				eval $cmd
 			fi
 		done
-		clr_yellow ">  " -n; clr_reset "$1\t" -n; clr_green "[INSTALLED]"
+		clr_cyan ">  " -n; clr_reset "$1\t" -n; clr_green "[INSTALLED]"
 	else
 		case "$1" in
 			"Homebrew")	brew update && brew upgrade ;;
 			"OhMyZsh")	env ZSH=$ZSH bash $ZSH/tools/check_for_upgrade.sh ;;
 			*)			true ;;
 		esac
-		clr_yellow ">  " -n; clr_reset "$1\t" -n; clr_yellow "[UPDATED]"
+		clr_cyan ">  " -n; clr_reset "$1\t" -n; clr_green "[UPDATED]"
 	fi
 }
 
@@ -37,14 +37,14 @@ function brew_install_cask() # Install app with homebrew if dont exists
 	brew cask list | grep $1 > /dev/null 2> /dev/null
 	if [ $? -ne 0 ]
 	then
-		clr_yellow "> " -n; clr_reset "Installing '" -n; clr_cyan "$1"
+		clr_cyan "> " -n; clr_reset "Installing cask '" -n; clr_magenta "$1"
 		case "$1" in
 			"iterm")	brew cask install iterm2 ;;
-			*)			brew cask install $1 ;;
+			*)		brew cask install $1 ;;
 		esac
-		clr_yellow ">  " -n; clr_reset "$1\t" -n; clr_green "[INSTALLED]"
+		clr_cyan ">  " -n; clr_reset "$1\t" -n; clr_green "[INSTALLED]"
 	else
-		clr_yellow ">  " -n; clr_reset "$1\t" -n; clr_yellow "[EXISTS]"
+		clr_cyan ">  " -n; clr_reset "$1\t" -n; clr_yellow "[EXISTS]"
 	fi
 }
 
@@ -56,11 +56,11 @@ function brew_install() # Install package with homebrew if dont exists
 	esac
 	if [ $? -ne 0 ]
 	then
-		clr_yellow ">  " -n; clr_reset "Installing " -n; clr_cyan "$1"
+		clr_cyan ">  " -n; clr_reset "Installing " -n; clr_magenta "$1"
 		brew install $1
-		clr_yellow ">  " -n; clr_reset "$1\t" -n; clr_green "[INSTALLED]"
+		clr_cyan ">  " -n; clr_reset "$1\t" -n; clr_green "[INSTALLED]"
 	else
-		clr_yellow ">  " -n; clr_reset "$1\t" -n; clr_yellow "[EXISTS]"
+		clr_cyan ">  " -n; clr_reset "$1\t" -n; clr_yellow "[EXISTS]"
 	fi
 }
 
