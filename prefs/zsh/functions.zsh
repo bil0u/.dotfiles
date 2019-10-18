3f8ebb0d41c8b698f6805955ab22a83b5d137fbc
Ugo Popée <ugo.popee@me.com>
2019-10-18 21:42:35 +0200

    Fixed yaml parser problem

--- prefs/zsh/functions.zsh
+++ prefs/zsh/functions.zsh
@@ -7,42 +7,52 @@
 function yaml_parse() # Parse the yaml file given as parameter and outputs the resulting variables
 {
 	local yaml_file=$1
-	local prefix=$2
-	local s
-	local w
-	local fs
+    local prefix=$2
+    local s
+    local w
+    local fs
 
-	s='[[:space:]]*'
-	w='[a-zA-Z0-9_.-]*'
-	fs="$(echo @|tr @ '\034')"
+    s='[[:space:]]*'
+    w='[a-zA-Z0-9_.-]*'
+    fs="$(echo @|tr @ '\034')"
 
-	(
-		sed -ne '/^--/s|--||g; s|\"|\\\"|g; s/\s*$//g;'\
-			-e "/#.*[\"\']/!s| #.*||g; /^#/s|#.*||g;"\
-			-e  "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p"\
-			-e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" |\
-		awk -F"$fs" '{
-			indent = length($1)/4;
-			if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
-			vname[indent] = $2;
-			for (i in vname) {if (i > indent) {delete vname[i]}}
-				if (length($3) > 0) {
-					vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-					printf("%s%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, conj[indent-1],$3);
-				}
-			}' |
+    (
+        sed -e '/- [^\“]'"[^\']"'.*: /s|\([ ]*\)- \([[:space:]]*\)|\1-\'$'\n''  \1\2|g' |
 
-		sed -e 's/_=/+=/g' \
-			-e '/\..*=/s|\.|_|' \
-			-e '/\-.*=/s|\-|_|'
+        sed -ne '/^--/s|--||g; s|\"|\\\"|g; s/[[:space:]]*$//g;' \
+            -e "/#.*[\"\']/!s| #.*||g; /^#/s|#.*||g;" \
+            -e "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+            -e "s|^\($s\)\($w\)${s}[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" |
 
-	) < "$yaml_file"
+        awk -F"$fs" '{
+            indent = length($1)/2;
+            if (length($2) == 0) { conj[indent]="+";} else {conj[indent]="";}
+            vname[indent] = $2;
+            for (i in vname) {if (i > indent) {delete vname[i]}}
+                if (length($3) > 0) {
+                    vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+                    printf("%s%s%s%s=(\"%s\")\n", "'"$prefix"'",vn, $2, conj[indent-1],$3);
+                }
+            }' |
+
+        sed -e 's/_=/+=/g' |
+
+        awk 'BEGIN {
+                FS="=";
+                OFS="="
+            }
+            /(-|\.).*=/ {
+                gsub("-|\\.", "_", $1)
+            }
+            { print }'
+    ) < "$yaml_file"
 }
 
 function yaml_vars() # Parse the yaml file given as parameter and set the resulting bash variables
 {
 	local yaml_file="$1"
-	eval "$(yaml_parse "$yaml_file")"
+    local prefix="$2"
+    eval "$(parse_yaml "$yaml_file" "$prefix")"
 }
 
 #
