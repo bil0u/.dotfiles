ee82cfcadfd90185e8a48a663bf1f72f2d207fa5
Ugo Popée <github@p0p.ee>
2023-02-14 15:36:48 +0100

    Supporting custom URLs in taps and args in cask installs

--- home/.chezmoitemplates/darwin/brew-install.sh
+++ home/.chezmoitemplates/darwin/brew-install.sh
@@ -1,6 +1,10 @@
 brew bundle --no-lock --file=/dev/stdin <<EOF
 {{- range (.taps | uniq) }}
+{{- if kindIs "string" . }}
 tap '{{ . }}'
+{{- else }}
+tap '{{ .name }}'{{ if hasKey . "url" }}, "{{ .url }}"{{ end }}
+{{- end }}
 {{- end }}
 # Formulaes
 {{- range (.formulaes | uniq) }}
@@ -8,7 +12,11 @@ brew '{{ . }}'
 {{- end }}
 # Casks
 {{- range (.casks | uniq) }}
+{{- if kindIs "string" . }}
 cask '{{ . }}'
+{{- else }}
+cask '{{ .name }}'{{ if hasKey . "args" }}, args: {{ .args }}{{ end }}
+{{- end }}
 {{- end }}
 # Mac AppStore
 {{- range (.mas | uniq) }}
